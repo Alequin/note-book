e68b0ee35de5b2a5cfae93af8562da753b0c2491
James Cox <j-cox03@hotmail.com>
2019-09-29 11:25:29 +0100

    Added logic to read tags from markdown files

--- scripts/build/build-json-notes-file/build-json-notes-file.js
+++ scripts/build/build-json-notes-file/build-json-notes-file.js
@@ -1,26 +1,31 @@
-const { startCase } = require('lodash')
-const fs = require('fs-extra')
-const allFilesInDirectory = require('./all-files-in-directory')
-const fileNameFromPath = require('./file-name-from-path')
-const readMarkdownFile = require('./read-markdown-file')
+const { startCase } = require("lodash");
+const fs = require("fs-extra");
+const allFilesInDirectory = require("./all-files-in-directory");
+const fileNameFromPath = require("./file-name-from-path");
+const readMarkdownFile = require("./read-markdown-file");
+const tags = require("./tags/tags");
 const {
   ROOTS_JSON_FILE,
-  RAW_NOTES_DIRECTORY
-} = require('../../../config/environment')
+  RAW_NOTES_DIRECTORY,
+} = require("../../../config/environment");
 
 const buildJsonNotesFile = async directory => {
-  const allFilesInNotesDir = await allFilesInDirectory(directory)
-  const filesWithTrimmedRoots = allFilesInNotesDir.map(filePath => ({
-    path: filePath.replace(`${directory}/`, ''),
-    name: startCase(fileNameWithoutExtension(filePath)),
-    content: readMarkdownFile(filePath)
-  }))
+  const allFilesInNotesDir = await allFilesInDirectory(directory);
+  const filesWithTrimmedRoots = allFilesInNotesDir.map(filePath => {
+    const content = readMarkdownFile(filePath);
+    return {
+      path: filePath.replace(`${directory}/`, ""),
+      name: startCase(fileNameWithoutExtension(filePath)),
+      content,
+      tags: tags(content),
+    };
+  });
 
-  fs.writeJsonSync(ROOTS_JSON_FILE, filesWithTrimmedRoots)
-}
+  fs.writeJsonSync(ROOTS_JSON_FILE, filesWithTrimmedRoots);
+};
 
 const fileNameWithoutExtension = filePath =>
-  fileNameFromPath(filePath).replace(/\..*$/, '')
+  fileNameFromPath(filePath).replace(/\..*$/, "");
 
-if (require.main === module) buildJsonNotesFile(RAW_NOTES_DIRECTORY)
-module.exports = buildJsonNotesFile
+if (require.main === module) buildJsonNotesFile(RAW_NOTES_DIRECTORY);
+module.exports = buildJsonNotesFile;
