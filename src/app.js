682ed6ed9461fe9d8e541463f9ed59e7d65b5d9a
James Cox <j-cox03@hotmail.com>
2019-09-30 22:52:29 +0100

    Made component to be the route-explorer

--- src/app.js
+++
@@ -1,5 +0,0 @@
-import React from 'react'
-
-const App = () => <div>Home</div>
-
-export default App
---
+++ src/route-explorer.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const RouteExplorer = () => {
+  return null;
+};
+
+export default RouteExplorer;
