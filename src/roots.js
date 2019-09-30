52838a026dba6f50d1516813235b7f48aa82c61a
James Cox <j-cox03@hotmail.com>
2019-09-30 09:49:21 +0100

    Used roots.json to create react routes in app

---
+++ src/roots.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import ROOTS from "./roots.json";
+
+const Roots = () => (
+  <Router>
+    {ROOTS.map(({ path, content }, index) => (
+      <Route
+        key={index}
+        exact
+        path={`/${path}`}
+        render={() => <div>{content}</div>}
+      />
+    ))}
+  </Router>
+);
+
+export default Roots;
