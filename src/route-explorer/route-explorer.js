7d53fe8d49ab5c4e2f6697cb1542ced76baa6464
James Cox <j-cox03@hotmail.com>
2019-10-02 00:18:59 +0100

    Made simple file explorer

--- src/route-explorer/route-explorer.js
+++ src/route-explorer/route-explorer.js
@@ -1,18 +1,97 @@
 import React, { useState } from "react";
+import { Link } from "react-router-dom";
 import styled from "styled-components";
+import sortBy from "lodash/sortBy";
 import filterRoutesToDisplay from "./filter-routes-to-display";
 import itemsToDisplay from "./items-to-display";
+
 import ROOTS from "../routes.json";
+import FILE_IMG from "./file.png";
+import DIRECTORY_IMG from "./folder.png";
+
+const useCurrentRoute = () => {
+  const [currentRoute, setCurrentRoute] = useState([]);
+
+  const addToCurrentRoute = newRouteSegment =>
+    setCurrentRoute([...currentRoute, newRouteSegment]);
+
+  return {
+    currentRoute,
+    addToCurrentRoute
+  };
+};
 
 const RouteExplorer = () => {
-  const [currentRoute, setCurrentRoute] = useState([]);
+  const { currentRoute, addToCurrentRoute } = useCurrentRoute();
 
   const directoriesAndFilesToDisplay = itemsToDisplay(
     filterRoutesToDisplay(currentRoute, ROOTS),
     currentRoute.length
   );
 
-  return null;
+  return (
+    <>
+      <CurrentRoute currentRoute={currentRoute} />
+      <ItemSection>
+        {sortBy(
+          directoriesAndFilesToDisplay,
+          ({ isDirectory }) => !isDirectory
+        ).map(({ isDirectory, name, to }, index) =>
+          isDirectory ? (
+            <Directory
+              key={index + name}
+              name={name}
+              onSelect={() => addToCurrentRoute(name)}
+            />
+          ) : (
+            <File key={index + name} name={name} to={to}></File>
+          )
+        )}
+      </ItemSection>
+    </>
+  );
 };
+
+const CurrentRoute = ({ currentRoute }) => (
+  <p>Current Path: /{currentRoute.join("/")}</p>
+);
+
+const Directory = ({ name, onSelect }) => (
+  <Item name={name} onSelect={onSelect} image={DIRECTORY_IMG} />
+);
+const File = ({ name, to }) => (
+  <Link to={to}>
+    <Item name={name} image={FILE_IMG} />
+  </Link>
+);
+
+const Item = ({ name, onSelect, image }) => (
+  <ItemBox
+    tabIndex={0}
+    onKeyPress={onSelect && (event => event.key === "Enter" && onSelect())}
+    onClick={onSelect && (() => onSelect())}
+  >
+    <img src={image}></img>
+    <p>Name: {name}</p>
+  </ItemBox>
+);
+
+const ItemSection = styled.section`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  grid-column-gap: 2vw;
+  grid-row-gap: 2vw;
+`;
+
+const ItemBox = styled.div`
+  text-align: center;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  &:hover {
+    border: 1px solid rgba(0, 0, 0, 0.3);
+  }
+  &:active {
+    border: 1px solid rgba(0, 0, 0, 0.3);
+  }
+`;
 
 export default RouteExplorer;
