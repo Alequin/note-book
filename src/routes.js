1ae1562f3514c01cf2a737008526a7527620c9e3
James Cox <j-cox03@hotmail.com>
2019-10-02 21:52:18 +0100

    Added catch all route to router

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,21 @@
 import React from "react";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import RouteExplorer from "./route-explorer/route-explorer";
 import ROOTS from "./routes.json";
 
 const Roots = () => (
   <Router>
-    <Route path="/route-explorer">
-      <RouteExplorer />
-    </Route>
-    {ROOTS.map(({ path, content }, index) => (
-      <Route key={index} exact path={path}>
-        <div>{content}</div>
+    <Switch>
+      <Route exact path="/route-explorer">
+        <RouteExplorer />
       </Route>
-    ))}
+      {ROOTS.map(({ path, content }, index) => (
+        <Route key={index} exact path={path}>
+          <div>{content}</div>
+        </Route>
+      ))}
+      <Route path="*">Route does not exist</Route>
+    </Switch>
   </Router>
 );
 
