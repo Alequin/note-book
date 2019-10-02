e2178bd146882864e12a5fc69bcd9687921972ad
James Cox <j-cox03@hotmail.com>
2019-10-02 23:11:14 +0100

    Change location of test folders

--- config/environment.js
+++ config/environment.js
@@ -1,20 +1,30 @@
 const path = require("path");
+const ROOT_DIR = `${__dirname}/..`;
 
 const isTestEnvironment = process.env.IS_TEST;
 
-const newFile = ({ forTests, standardPath }) =>
-  isTestEnvironment ? path.resolve(forTests) : path.resolve(standardPath);
+const newPath = ({ forTests, standardPath }) =>
+  isTestEnvironment && forTests
+    ? path.resolve(`${ROOT_DIR}/${forTests}`)
+    : path.resolve(`${ROOT_DIR}/${standardPath}`);
 
-const RAW_NOTES_DIRECTORY = isTestEnvironment
-  ? path.resolve("./test-raw-notes")
-  : path.resolve("./raw-notes");
+const RAW_NOTES_DIRECTORY = newPath({
+  forTests: "./mock-dirs/test-raw-notes",
+  standardPath: "./raw-notes"
+});
 
-const ROUTES_JSON_FILE = newFile({
-  forTests: "./test-routes.json",
+const ROUTES_JSON_FILE = newPath({
+  forTests: "./mock-dirs/test-routes.json",
   standardPath: "./src/routes.json"
+});
+
+const ASSETS_DIRECTORY = newPath({
+  forTests: "./mock-dirs/test-assets",
+  standardPath: "./assets"
 });
 
 module.exports = Object.freeze({
   RAW_NOTES_DIRECTORY,
-  ROUTES_JSON_FILE
+  ROUTES_JSON_FILE,
+  ASSETS_DIRECTORY
 });
--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
-const fs = require('fs-extra')
-process.env.IS_TEST = true
+const fs = require("fs-extra");
+process.env.IS_TEST = true;
 // Clean up any old test raw-notes files and make a clean test dir for raw-notes tests
-fs.existsSync('./test-raw-notes')
-  ? fs.emptyDirSync('./test-raw-notes')
-  : fs.mkdirSync('./test-raw-notes')
+fs.existsSync("./mock-dirs")
+  ? fs.emptyDirSync("./mock-dirs")
+  : fs.mkdirSync("./mock-dirs");
