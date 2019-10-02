8c99df1d38d72bcbf598dca140fb2eeeecc468fa
James Cox <j-cox03@hotmail.com>
2019-10-02 23:09:47 +0100

    Changed file-path-manipulation package name

---
+++ local-packages/file-path/directory-contents-with-resolved-paths.test.js
@@ -0,0 +1,32 @@
+const directoryContentsWithResolvedPaths = require("./directory-contents-with-resolved-paths");
+const fs = require("fs-extra");
+const { RAW_NOTES_DIRECTORY } = require("../../config/environment");
+
+describe("directoryContentsWithResolvedPaths", () => {
+  afterEach(() => {
+    fs.emptyDirSync(RAW_NOTES_DIRECTORY);
+  });
+
+  it("identfies filepaths in the given directory", () => {
+    const rootDirForTest = `${RAW_NOTES_DIRECTORY}/test-lvl-1`;
+    const filePath1 = `${rootDirForTest}/simple-markdown.md`;
+    fs.outputFileSync(filePath1, "");
+    const filePath2 = `${rootDirForTest}/test-lvl-2`;
+    fs.mkdirSync(filePath2);
+
+    const expected = [filePath1, filePath2];
+    const actual = directoryContentsWithResolvedPaths(rootDirForTest);
+    expect(actual).toEqual(expected);
+  });
+
+  it("Ignores private files", () => {
+    const privateFileName = ".private-file-to-ignore";
+    const privateFileDir = `${RAW_NOTES_DIRECTORY}/test-lvl-1-1`;
+    const privateFilePath = `${privateFileDir}/${privateFileName}`;
+
+    fs.outputFileSync(privateFilePath);
+
+    const actual = directoryContentsWithResolvedPaths(privateFileDir);
+    expect(actual).toEqual([]);
+  });
+});
