6299e64686144dc9bc7a53aad32d580be1f4355e
James Cox <j-cox03@hotmail.com>
2019-10-11 09:43:13 +0100

    Created a very simple input for adding search tags

--- src/tag-explorer/tag-explorer.js
+++ src/tag-explorer/tag-explorer.js
@@ -1,7 +1,67 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
+import styled from "styled-components";
+
+import ROOTS from "../routes.json";
+
+const useTags = () => {
+  const [tagInputText, setTagInputText] = useState("");
+  const [tagsToSearch, setTagsToSearch] = useState([]);
+
+  const addTag = useCallback(() => {
+    setTagsToSearch([...tagsToSearch, tagInputText]);
+    setTagInputText("");
+  }, [tagsToSearch, tagInputText]);
+
+  const removeTag = useCallback(
+    tagToRemove =>
+      setTagInputText(tagsToSearch.filter(tag => tag !== tagToRemove)),
+    [tagsToSearch]
+  );
+
+  const updateTagInputText = useCallback(
+    ({ target: { value } }) => setTagInputText(value),
+    []
+  );
+
+  return { tagInputText, tagsToSearch, addTag, removeTag, updateTagInputText };
+};
 
 const TagExplorer = () => {
-  return null;
+  const {
+    tagInputText,
+    tagsToSearch,
+    addTag,
+    removeTag,
+    updateTagInputText
+  } = useTags();
+
+  return (
+    <>
+      <input
+        type="search"
+        value={tagInputText}
+        onChange={updateTagInputText}
+        on
+      ></input>
+      <button onClick={addTag}>add Tag</button>
+      <p>{tagInputText}</p>
+      <ActiveTagSection>
+        {tagsToSearch.map(tag => (
+          <ActiveTag>{tag}</ActiveTag>
+        ))}
+      </ActiveTagSection>
+    </>
+  );
 };
+
+const ActiveTagSection = styled.section`
+  display: flex;
+`;
+
+const ActiveTag = styled.div`
+  padding: 0.2rem;
+  margin: 0.1rem;
+  background: cyan;
+`;
 
 export default TagExplorer;
