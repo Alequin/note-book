dc71ff8027032818da22f1bc41f93b685e37087f
James Cox <j-cox03@hotmail.com>
2019-10-13 18:05:28 +0100

    ensured item buttons are never different heights on route-explorer

--- src/components/item-button.js
+++ src/components/item-button.js
@@ -11,8 +11,12 @@ const ItemButton = ({ name, onClick, image }) => (
 );
 
 const Button = styled(BlankButton)`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   text-align: center;
   width: 100%;
+  height: 100%;
   padding: 0;
   background: transparent;
   font-size: 1rem;
